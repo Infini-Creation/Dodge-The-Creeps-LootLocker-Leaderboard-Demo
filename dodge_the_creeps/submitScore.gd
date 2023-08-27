598e4115acb47a7a25693cf1a56bc8515a6fec72
Infini-Creation <infini.creation.france@gmail.com>
2023-08-27 16:37:26 +0200

    Update submitScore.gd
    -move code from ready function to _on_line_edit_visibility_changed
    -add call to set_name LootLocker function if name is not yet defined or as change since last time
    -use "player_name" instead of old "Name" player's name data
    -remove hiding on the mai container (old test code)

--- dodge_the_creeps/submitScore.gd
+++ dodge_the_creeps/submitScore.gd
@@ -6,9 +6,12 @@ extends Control
 signal score_submitted(status: bool)
  
 func _ready():
-	$VBoxContainer/LineEdit.text = Global.PLAYER_DATA["Name"]
-	if $VBoxContainer/LineEdit.text != "":
-		$VBoxContainer/Button.disabled = false
+	print("submit score: _ready called")
+	#lineedit not initialised with right value !  calledoo early (too)
+	#print("=>> pn="+Global.PLAYER_DATA["player_name"])
+	#$VBoxContainer/LineEdit.text = Global.PLAYER_DATA["player_name"]
+	#if $VBoxContainer/LineEdit.text != "":
+		#$VBoxContainer/Button.disabled = false
 
 
 func set_score(score : int) -> void:
@@ -18,9 +21,18 @@ func set_score(score : int) -> void:
 
 func _on_button_pressed():
 	Global.PLAYER_DATA["Name"] = $VBoxContainer/LineEdit.text
+	var result : int
+	
+	if LootLocker.current_user.PLAYERSDATA["player_name"] == "" or LootLocker.current_user.PLAYERSDATA["player_name"] != Global.PLAYER_DATA["Name"]:
+		print("update player's name")
+		result = await LootLocker.current_user.set_player_name(Global.PLAYER_DATA["Name"])
+		if result == OK:
+			print("player's name set successfully")
+		else:
+			print("Error setting player's name !")
 
-	print("Will submit score: "+str(Score)+" from player: "+Global.PLAYER_DATA["Name"])
-	var result = await LootLocker.leaderboard.submit_score(Score)
+	print("Will submit score: "+str(Score)+" from player: "+Global.PLAYER_DATA["player_name"])
+	result = await LootLocker.leaderboard.submit_score(Score)
 	print("submit_score res="+str(result))
 	if result == OK:
 		print("score submitted successfully")
@@ -33,12 +45,14 @@ func _on_button_pressed():
 	#if playerName != "":
 		#player_name_changed.emit(playerName)
 
-	$VBoxContainer.hide()
+	##$VBoxContainer.hide()
 
 
 func _on_line_edit_text_submitted(new_text : String):
-	Global.PLAYER_DATA["Name"] = new_text
+	Global.PLAYER_DATA["player_name"] = new_text
 	print("new name set:"+new_text)
+	#in player data, either dirty = [name] or pn updated, then above, only if updated => setname call
+	#laos upon login if name != global player name => setname
 	if new_text != "":
 		$VBoxContainer/Button.disabled = false
 	else:
@@ -50,3 +64,11 @@ func _on_line_edit_text_changed(new_text : String):
 		$VBoxContainer/Button.disabled = false
 	else:
 		$VBoxContainer/Button.disabled = true
+
+
+func _on_line_edit_visibility_changed():
+	if $VBoxContainer/LineEdit.is_visible_in_tree() == true:
+		print("=>> pn="+Global.PLAYER_DATA["player_name"])
+		$VBoxContainer/LineEdit.text = Global.PLAYER_DATA["player_name"]
+		if $VBoxContainer/LineEdit.text != "":
+			$VBoxContainer/Button.disabled = false
