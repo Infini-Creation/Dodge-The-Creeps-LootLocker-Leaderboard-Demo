bfc10ed5598efb89254bbe978be907a39494812f
Infini-Creation <infini.creation.france@gmail.com>
2023-09-23 10:17:53 +0200

    Update score_item.gd
    add highlight item line function

--- dodge_the_creeps/score_item.gd
+++ dodge_the_creeps/score_item.gd
@@ -9,9 +9,15 @@ func setup(rank: int, playerName: String, score : int) -> void:
 	Rank = rank
 	Name = playerName
 	Score = score
-	
+
 
 func _ready():
 	$HBoxContainer/Rank.text = str(Rank)
 	$HBoxContainer/Name.text = Name
 	$HBoxContainer/Score.text = str(Score)
+
+
+func highlight_entry(color: Color) -> void:
+	$HBoxContainer/Rank.set("theme_override_colors/font_color", color)
+	$HBoxContainer/Name.set("theme_override_colors/font_color", color)
+	$HBoxContainer/Score.set("theme_override_colors/font_color", color)
