66bc7b0522447c48a40ba71629b71ae7fa4ff75e
Infini-Creation <infini.creation.france@gmail.com>
2023-09-23 10:17:29 +0200

    Update Main.gd
    rewrite the load/save process to make it work with web export (itch.io mostly)

--- dodge_the_creeps/Main.gd
+++ dodge_the_creeps/Main.gd
@@ -7,76 +7,77 @@ extends Node
 var score
 var do_not_save_data : bool = false
 
-var ignore_first_close_notification : bool = true
+#var ignore_first_close_notification : bool = true
 
-signal player_data_saved
-
-func _init():
-	pass
+#signal player_data_saved
 
 
 func _ready():
 	print("ready called")
 	$MobTimer.wait_time = default_mob_time
 	
-	get_tree().set_auto_accept_quit(false)
-	player_data_saved.connect(_on_data_saved)
-	$HUD.do_no_save_data_on_exit.connect(_on_dontsave_received)
+	#get_tree().set_auto_accept_quit(false)
+	#player_data_saved.connect(_on_data_saved)
+	#$HUD.do_no_save_data_on_exit.connect(_on_dontsave_received)
 
-	load_player_data()
+	Global.load_player_data()
 	
 	print("data="+str(Global.PLAYER_DATA))
 
-	var lok = LootLockerDataVault.load_sdk_data()
-	print("lok ="+str(lok))
+	LootLocker.setup("dev_529b11593cd64732b9c97123913e2ee2", "e6m82yag", "0.0.0.1", true)
+	#var lok = LootLockerDataVault.load_sdk_data()
+	#print("lok ="+str(lok))
 	#setupfromfile should be use to don't need to deal with this ourselves
-	LootLocker.setup(LootLockerDataVault.API_KEY, LootLockerDataVault.DOMAIN_KEY, LootLockerDataVault.GAME_VERSION, true, true)
+	#LootLocker.setup(LootLockerDataVault.API_KEY, LootLockerDataVault.DOMAIN_KEY, LootLockerDataVault.GAME_VERSION, true, true)
 
 	#load/save => easy: load data = something ? Y => replace default
 		#then save on exit (old value or new one, like session after is exists
 
-	print("logging in...")
-		#input here should be palyer id (long like bd5d5d12-0cd7-429a-9175-d04406a97eea)
-	var result = await LootLocker.guest_login(Global.PLAYER_DATA["player_identifier"])
-	if result == OK:
-		print("logged in, sessionT="+LootLocker.session.token)
-		print("PLayerID="+LootLocker.current_user.PLAYERSDATA["player_identifier"])
-		print("PLayerName="+LootLocker.current_user.PLAYERSDATA["player_name"])
-		Global.PLAYER_DATA["player_identifier"] = LootLocker.current_user.PLAYERSDATA["player_identifier"]
-		Global.PLAYER_DATA["player_id"] = LootLocker.current_user.PLAYERSDATA["player_id"]
-		Global.PLAYER_DATA["public_uid"] = LootLocker.current_user.PLAYERSDATA["public_uid"]
-		Global.PLAYER_DATA["SessionToken"] = LootLocker.session.token
-		
-		LootLocker.add_leaderboard({"key": Global.LEADERBOARD_KEY, "session-token": LootLocker.session.token})
+	Global.login()
+	#print("logging in...")
+		##input here should be player id (long like bd5d5d12-0cd7-429a-9175-d04406a97eea)
+	#var result = await LootLocker.guest_login(Global.PLAYER_DATA["player_identifier"])
+	#if result == OK:
+		#print("logged in, sessionT="+LootLocker.session.token)
+		#print("PLayerID="+LootLocker.current_user.PLAYERSDATA["player_identifier"])
+		#print("PLayerName="+LootLocker.current_user.PLAYERSDATA["player_name"])
+		#Global.PLAYER_DATA["player_identifier"] = LootLocker.current_user.PLAYERSDATA["player_identifier"]
+		#Global.PLAYER_DATA["player_id"] = LootLocker.current_user.PLAYERSDATA["player_id"]
+		#Global.PLAYER_DATA["public_uid"] = LootLocker.current_user.PLAYERSDATA["public_uid"]
+		#Global.PLAYER_DATA["SessionToken"] = LootLocker.session.token
+#
+		#Global.save_player_data()
+#
+		#LootLocker.add_leaderboard({"key": Global.LEADERBOARD_KEY, "session-token": LootLocker.session.token})
+#
+	#else:
+		#print("ERROR ! Not logged in")
 
-	else:
-		print("ERROR ! Not logged in")
+#func _notification(what):
+	#if what == NOTIFICATION_WM_CLOSE_REQUEST:
+			#print("WM close req notif")
+			#print("action...")
+#
+			#call_deferred("queue_free")
+#
+			#print("_notification wait...")
+			#await get_tree().create_timer(1).timeout #tmp  without it, nothing output after "_input wait"
+				##with it, "spd called" then... nothing
+			#print("_notification delay completed, quit") #<= never reached ??
+#
+			#get_tree().quit()
+#
 
-func _notification(what):
-	if what == NOTIFICATION_WM_CLOSE_REQUEST:
-			print("WM close req notif")
-			print("action...")
-			
-			call_deferred("queue_free")
-			
-			print("_notification wait...")
-			await get_tree().create_timer(1).timeout #tmp  without it, nothing output after "_input wait"
-				#with it, "spd called" then... nothing
-			print("_notification delay completed, quit") #<= never reached ??
-			
-			get_tree().quit()
+#func _input(event):
+	#if event.is_action_pressed("quit"):
+		#print("quit action")
 		
-
-func _input(event):
-	if event.is_action_pressed("quit"):
-		print("quit action")
-		
-		print("_input wait...")
-		await get_tree().create_timer(2).timeout #tmp
-		print("_input delay completed, send notif")
-		
-		#this get very quick and prevent previous statement to be "seen"
-		get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+		#print("_input wait...")
+		#await get_tree().create_timer(2).timeout #tmp
+		#print("_input delay completed, send notif")
+#
+		##this get very quick and prevent previous statement to be "seen"
+		#get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 		#pass
 
 
@@ -91,6 +92,7 @@ func game_over():
 func new_game():
 	get_tree().call_group(&"mobs", &"queue_free")
 	score = 0
+	Global.login()
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
@@ -134,45 +136,6 @@ func _on_StartTimer_timeout():
 	$ScoreTimer.start()
 
 
-func load_player_data() -> void :
-	var data_read : String
-	
-	if !FileAccess.file_exists("user://dtc.data"):
-		return
-
-	var file = FileAccess.open("user://dtc.data", FileAccess.READ)
-	if file != null:
-		for item in Global.PLAYER_DATA:
-			data_read = file.get_pascal_string ()
-			print("load_player_data: "+item+"="+data_read)
-			if data_read != "":
-				Global.PLAYER_DATA[item] = data_read
-		file.close()
-	else:
-		print("Error opening file: "+str(file.get_error()))
-
-
-func save_player_data():
-	print("spd called")
-
-	if do_not_save_data == false:
-		var file = FileAccess.open("user://dtc.data", FileAccess.WRITE)
-		if file != null:
-			for item in Global.PLAYER_DATA:
-				if item != "_dirty":
-					print("save item "+item+"="+str(Global.PLAYER_DATA[item]))
-					#if Global.PLAYER_DATA[item] != "": #should check updated flag (to add)
-					file.store_pascal_string (str(Global.PLAYER_DATA[item]))
-					if file.get_error():
-						print("Error storing data ["+item+"]: "+str(file.get_error()))
-			file.close()
-		else:
-			print("Error opening file: "+str(file.get_error()))
-			
-	print("spd completed")
-	player_data_saved.emit()
-
-
 ## add bonus (hector)
 # effect: invincibility for x secs
 #	can eat mobs (contact make them disappear)
@@ -184,16 +147,16 @@ func save_player_data():
 #	tinier mobs
 
 #no called cause close window = "hard kill" maybe, try to add some get_tree().exit_tree
-func _on_tree_exiting():
-	print("_on_tree_exiting")
-	save_player_data()
+#func _on_tree_exiting():
+	#print("_on_tree_exiting")
+	#save_player_data()
 	
-func _exit_tree():
-	print("_exit_tree called")
+#func _exit_tree():
+	#print("_exit_tree called")
 
-func _on_data_saved():
-	print("data saved, quit")
-	get_tree().quit()
+#func _on_data_saved():
+	#print("data saved, quit")
+	#get_tree().quit()
 
 func _on_dontsave_received():
 	print("data won't be saved on exit")
